588dde24181cb620d8b0ff86487211fcf5311985
cirbal <cirbal@PcirbalB>
2022-10-13 01:49:17 -0300

    Clase Productos ApplicationProperties - port,spring name

---
+++ src/main/java/com/cirbal/app/productos/models/entity/Producto.java
@@ -0,0 +1,70 @@
+package com.cirbal.app.productos.models.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.annotation.Generated;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "producto")
+public class Producto implements Serializable{
+
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "name")
+	private String nombre;
+	@Column(name="price")
+	private Double precio;
+	@Column(name = "create_at")
+	@Temporal(TemporalType.DATE)
+	private Date createAt;
+	
+	
+	public Producto(Long id, String nombre, Double precio, Date createAt) {
+		this.id = id;
+		this.nombre = nombre;
+		this.precio = precio;
+		this.createAt = createAt;
+	}
+	
+	public Producto() {
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+	public Date getCreateAt() {
+		return createAt;
+	}
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+	
+	private static final long serialVersionUID = 4637505574426072441L;
+	
+}
